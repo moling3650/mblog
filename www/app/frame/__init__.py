2776d9cc6ab1048adbb850a46ac954a40b992178
moling <365024424@qq.com>
2016-06-01 18:57:43 +0800

    add debug code in RequestHandler

--- www/app/frame/__init__.py
+++ www/app/frame/__init__.py
@@ -54,9 +54,11 @@ class RequestHandler(object):
         required_args = inspect.signature(self._func).parameters
         logging.info('required args: %s' % str(required_args))
 
+        result = await self.get_args(request)
+        if isinstance(result, web.HTTPBadRequest):
+            return result
         # 获取从GET或POST传进来的参数值，如果函数参数表有这参数名就加入
-        args = await self.get_args(request)
-        kw = {arg: value for arg, value in args.items() if arg in required_args}
+        kw = {arg: value for arg, value in result.items() if arg in required_args}
 
         # 获取match_info的参数值，例如@get('/blog/{id}')之类的参数值
         kw.update(dict(**request.match_info))
@@ -65,7 +67,14 @@ class RequestHandler(object):
         if 'request' in required_args:
             kw['request'] = request
 
-        self.check_args(required_args, kw)
+        # 检查参数表中有没参数缺失
+        for arg in required_args.values():
+            # 如果参数类型不是变长列表和变长字典，变长参数是可缺省的
+            if arg.kind not in (arg.VAR_POSITIONAL, arg.VAR_KEYWORD):
+                # 如果还是没有默认值，而且还没有传值的话就报错
+                if arg.default == arg.empty and arg.name not in kw:
+                    return web.HTTPBadRequest(text='Missing argument: %s' % arg.name)
+
         logging.info('call with args: %s' % str(kw))
         try:
             return await self._func(**kw)
@@ -75,23 +84,24 @@ class RequestHandler(object):
     async def get_args(self, request):
         # 从POST方法截取数据
         if request.method == 'POST':
-            if request.content_type.startswith('application/json'):
-                return await request.json()
-            elif request.content_type.startswith('application/x-www-form-urlencoded'):
-                return await request.post()
+            if not request.content_type:
+                return web.HTTPBadRequest(text='Missing Content-Type.')
+            ct = request.content_type.lower()
+            if ct.startswith('application/json'):
+                params = await request.json()
+                if not isinstance(params, dict):
+                    return web.HTTPBadRequest(text='JSON body must be object.')
+                return params
+            elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    return dict(**params)
+            else:
+                return web.HTTPBadRequest(text='Unsupported Content-Type: %s' % request.content_type)
         # 从GET方法截取数据，例如/?page=2
         elif request.method == 'GET':
             qs = request.query_string
             return {k: v[0] for k, v in parse.parse_qs(qs, True).items()}
         return dict()
-
-    def check_args(self, required_args, args):
-        for arg in required_args.values():
-            # 如果参数类型不是变长列表和变长字典，变长参数是可缺省的
-            if arg.kind not in (arg.VAR_POSITIONAL, arg.VAR_KEYWORD):
-                # 如果还是没有默认值，而且还没有传值的话就报错
-                if arg.default == arg.empty and arg.name not in args:
-                    return web.HTTPBadRequest('Missing argument: %s' % arg.name)
 
 
 # 添加一个模块的所有路由
